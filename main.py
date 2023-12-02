76fefb3cd5211dd810d42281c6ca335a33758dea
hurmong <hurmong@hanmail.net>
2023-12-03 00:02:28 +0900

    2023.12.02

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-from adb_extract import *
-from cache_image import *
-from clipboard import *
-from full_shared_prefs import *
-from contacts import *
-from usage import *
-import pandas as pd
-def main():
-    #adb extract usage
-    # phone_type=input("폰 기종을 입력해수세요(1. 갤럭시 2.픽셀) : ")
-    android_version=int(get_androidVersion())
-    # if android_version<11:
-    #     destination_dir='/data/system/' # usagestats 경로 유의하기 
-    # else:
-    #     destination_dir='/data/system_ce/0' 
-    # usage='usagestats'
-    # extract_data(usage,destination_dir)
-    wiping_check,wiping_application=usagestats(android_version)#package_name도  추출해야함
+from modules import adb_extract
+from modules import cache
+from modules import clipboard
+from modules import shared_prefs
+from modules import contacts
+from modules import usage
+import os
 
+def main():
+    if not os.path.exists('result'):
+        os.makedirs('result')
+    #adb extract usage
+    phone_type=input("폰 기종을 입력해수세요(1. 갤럭시 2.픽셀) : ")
+    android_version=int(adb_extract.get_androidVersion())
+    # android_version=13
+    if android_version<11:
+        destination_dir='/data/system/' # usagestats 경로 유의하기 
+    else:
+        destination_dir='/data/system_ce/0' 
+    usage_name='usagestats'
+    adb_extract.extract_data(usage_name,destination_dir)
+    wiping_check,wiping_application=usage.usagestats(android_version)#package_name도  추출해야함
+
+    # wiping_application=['com.shredder.fileshredder.securewipe','com.palmtronix.shreddit.v1']
+    # wiping_check=False
     if wiping_check==True:
         print("wiping 흔적 없음")
         exit(0)
     else:
-        print(wiping_application)
         #adb extract clipboard data
         destination_dir='/data'
         if android_version<11:
@@ -33,41 +38,41 @@ def main():
             clipboard_folder='com.samsung.android.honeyboard'
             clipboard_name='databases/ClipItem.db'
 
-        # if phone_type==2:
-        #     clipboard_folder='com.google.android.inputmethod.latin'
-        # extract_data(clipboard_folder,destination_dir)
+        if phone_type==2:
+            clipboard_folder='com.google.android.inputmethod.latin'
+        adb_extract.extract_data(clipboard_folder,destination_dir)
 
         # #adb extract all data except clipboard data
-        # data_list=['com.sec.android.gallery3d','com.samsung.android.providers.contacts']
-        # data_list.extend(wiping_application)
-        # destination_dir='/data/data'
-        # for file_name in data_list:
-        #     extract_data(file_name,destination_dir)
+        data_list=['com.sec.android.gallery3d','com.samsung.android.providers.contacts']
+        data_list.extend(wiping_application)
+        destination_dir='/data/data'
+        for file_name in data_list:
+            adb_extract.extract_data(file_name,destination_dir)
 
 
         #gallery cache
-        # gallery_destination_dir='./extractdata'
-        # gallery_renamed_files_dir='./extractdata/gallery3d_cache'
-        # cache_image(gallery_destination_dir,gallery_renamed_files_dir)
+        gallery_destination_dir='./extractdata'
+        gallery_renamed_files_dir='./extractdata/gallery3d_cache'
+        cache.cache_image(gallery_destination_dir,gallery_renamed_files_dir)
 
         #clipboard
-        # if android_version<11:
-        #     clipboardFile_wiping('경로 지정')
-        # else:
-        #     clipboardDB_wiping('extractdata',clipboard_folder,clipboard_name)
+        if android_version<11:
+            clipboard.clipboardFile_wiping('경로 지정')
+        else:
+            clipboard.clipboardDB_wiping('extractdata',clipboard_folder,clipboard_name)
 
         #contacts
-        # contacts=contacts_wiping('extractdata','com.samsung.android.providers.contacts/databases/contacts2.db')
-        # print(contacts)
+        contacts.contacts_wiping('extractdata','com.samsung.android.providers.contacts/databases/contacts2.db')
 
-        #application folder
+
+        # application folder
         for package_name in wiping_application:
-            full_shared_prefs('extractdata',package_name)
+            shared_prefs.full_shared_prefs('extractdata',package_name)
+            folder_destination_dir=f'./extractdata/{package_name}/image_manage_disk_cache'
+            folder_renamed_files_dir=f'./result/{package_name}'
+            cache.cache_image(folder_destination_dir,folder_renamed_files_dir)
+        
 
-        if 'securewipe' or 'com.palmtronix.shreddit.v1' in wiping_application: # 애플리케이션 보고서 존재
-            folder_destination_dir='./'
-            folder_renamed_files_dir='(수정바람)'
-            cache_image(folder_destination_dir,folder_renamed_files_dir)
 
 if __name__=="__main__":
     main()
---
+++ modules/contacts.py
@@ -0,0 +1,29 @@
+import sqlite3
+import pandas as pd
+import os 
+
+def contacts_wiping(destination_dir,db_name):
+    # 파일 이름 고치기
+    db_path=os.path.join(destination_dir,db_name)
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT strftime('%Y-%m-%d %H:%M:%S', datetime(dc.contact_deleted_timestamp / 1000, 'unixepoch', 'localtime')) AS local_timestamp, "
+        "rc.sec_trash_caller_package, "
+        "MAX(CASE WHEN d.mimetype_id = 5 THEN d.data1 ELSE NULL END) AS cutom_value1, "
+        "MAX(CASE WHEN d.mimetype_id = 7 THEN d.data1 ELSE NULL END) AS cutom_value2 "
+        "FROM deleted_contacts dc "
+        "JOIN raw_contacts rc ON dc.contact_id = rc._id "
+        "LEFT JOIN data d ON rc._id = d.raw_contact_id "
+        "WHERE sec_trash_caller_package != ' com.samsung.android.dialer ' AND sec_trash_caller_package != ' None ' "
+        "GROUP BY dc.contact_deleted_timestamp, rc._id, rc.sec_trash_caller_package "
+        "ORDER BY dc.contact_deleted_timestamp;"
+    )
+
+    rows=cur.fetchall()
+    cols=[column[0] for column in cur.description]
+    contacts_df=pd.DataFrame.from_records(data=rows,columns=cols)
+    conn.close()
+    contacts_df.to_csv('./result/contacts.csv')
+
