43049a61fd6eec1214f752ad9d859304a8478772
YeasongJo <yeasong153@gmail.com>
2023-12-12 15:02:24 +0900

    tob_bar

--- modules/ui/view_data.py
+++ modules/ui/view_data.py
@@ -9,6 +9,73 @@ from modules.ui.view_dialog import InputDialog #1204 joys
 from modules.data_processing.report import WriteReport #1207 joys
 from modules.ui import ProcessBar
 from modules.ui.FinishWidget import FinishWidget
+
+class CustomTitleBar(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(CustomTitleBar, self).__init__(parent)
+        self.setFixedHeight(38)  # Title bar height
+        self.layout = QtWidgets.QHBoxLayout(self)
+        self.layout.setContentsMargins(10, 0, 10, 0)  # Margin: top, left, bottom, right
+        self.layout.setSpacing(0)
+        self.layout.setAlignment(QtCore.Qt.AlignCenter)  # Center items in the title bar
+
+        # App Icon
+        self.iconLabel = QtWidgets.QLabel(self)
+        icon = QtGui.QPixmap('./logo_Bokgumagic.png').scaled(24, 24, QtCore.Qt.KeepAspectRatio)
+        self.iconLabel.setPixmap(icon)
+        self.layout.addWidget(self.iconLabel, alignment=QtCore.Qt.AlignLeft)
+
+        # Title Label
+        self.titleLabel = QtWidgets.QLabel(" Bokgumagic_v2.0                                                                                                                                                                                                                                              ", self)
+        self.titleLabel.setFont(QtGui.QFont("맑은 고딕", 12))
+        self.layout.addWidget(self.titleLabel, alignment=QtCore.Qt.AlignLeft)
+        
+        # Spacing between title and buttons
+        self.layout.addStretch()
+
+        # Minimize Button
+        self.minimizeButton = QtWidgets.QPushButton("__", self)
+        self.minimizeButton.setFixedSize(45, 30)
+        self.layout.addWidget(self.minimizeButton, alignment=QtCore.Qt.AlignRight)
+
+        # Maximize/Restore Button
+        self.maximizeButton = QtWidgets.QPushButton("□", self)
+        self.maximizeButton.setFixedSize(45, 30)
+        self.layout.addWidget(self.maximizeButton, alignment=QtCore.Qt.AlignRight)
+
+        # Close Button
+        self.closeButton = QtWidgets.QPushButton("X", self)
+        self.closeButton.setFixedSize(45, 30)
+        self.layout.addWidget(self.closeButton, alignment=QtCore.Qt.AlignRight)
+
+        # Set the background color for the title bar and buttons
+        self.setStyleSheet("""
+            CustomTitleBar, QLabel, QPushButton {
+                background-color: rgb(224, 255, 255);
+                border: none;
+            }
+            QPushButton:hover {
+                background-color: rgb(200, 240, 240);
+            }
+            QPushButton:pressed {
+                background-color: rgb(180, 230, 230);
+            }
+        """)
+
+        # Connect button signals to their respective slots
+        self.minimizeButton.clicked.connect(parent.showMinimized)
+        self.maximizeButton.clicked.connect(self.toggleMaximizeRestore)
+        self.closeButton.clicked.connect(parent.close)
+
+    def toggleMaximizeRestore(self):
+        # Toggle maximize/restore on button click
+        if self.parent().isMaximized():
+            self.parent().showNormal()
+            self.maximizeButton.setText("□")
+        else:
+            self.parent().showMaximized()
+            self.maximizeButton.setText("❐")
+
 class ViewData(QMainWindow):
         def __init__(self,parent):
                 super().__init__()
@@ -28,6 +95,14 @@ class ViewData(QMainWindow):
                 icon = QtGui.QIcon()
                 icon.addPixmap(QtGui.QPixmap("./logo_Bokgumagic.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
                 self.setWindowIcon(icon)
+
+                # 기본 제목 표시줄 숨기기
+                self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        
+                # 사용자 지정 제목 표시줄 추가
+                self.customTitleBar = CustomTitleBar(self)
+                self.setMenuWidget(self.customTitleBar)
+
                 #전체화면 layout
                 self.centralLayout = QtWidgets.QHBoxLayout(self.centralwidget)
                 self.centralLayout.setContentsMargins(5, 5, 5, 5)
@@ -271,8 +346,6 @@ class ViewData(QMainWindow):
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    self = QtWidgets.Qself()
-    ui = ViewData()
-    ui.setupUi(self)
-    self.show()
+    mainWin = ViewData(None)
+    mainWin.show()
     sys.exit(app.exec_())
